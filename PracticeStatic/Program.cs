4122e94f1af286ef946a2e6cce74da68bf1a9103
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-06-11 14:46:09 +0400

    Add project files.

---
+++ PracticeStatic/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace PracticeStatic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[3];
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = CreateUser();
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Please select a command.");
+                Console.WriteLine("1. Show all users");
+                Console.WriteLine("2. Get information by ID");
+                Console.WriteLine("0. Exit");
+
+                string command = Console.ReadLine();
+
+                if (command == "1")
+                {
+                    ShowAllUsers(users);
+                }
+                else if (command == "2")
+                {
+                    Console.WriteLine("Enter user ID:");
+                    int userId;
+
+                    if (int.TryParse(Console.ReadLine(), out userId))
+                    {
+                        User user = User.FindUserById(users, userId);
+                        if (user != null)
+                        {
+                            Console.WriteLine($"Id = {user.Id}, FullName = {user.FullName}, Email = {user.Email}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("User not found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid user ID.");
+                    }
+                }
+                else if (command == "0")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command. Please enter a valid command.");
+                }
+            }
+        }
+
+        public static User CreateUser()
+        {
+            Console.WriteLine("Enter fullname:");
+            string fullName = Console.ReadLine();
+
+            Console.WriteLine("Enter email address:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter password:");
+            string password = Console.ReadLine();
+
+            return new User(fullName, email, password);
+        }
+
+
+        public static void ShowAllUsers(User[] users)
+        {
+            foreach (User user in users)
+            {
+                Console.WriteLine($"Id = {user.Id}, FullName = {user.FullName}, Email = {user.Email}");
+            }
+        }
+    }
+}
---
+++ PracticeStatic/User.cs
@@ -0,0 +1,74 @@
+﻿
+namespace PracticeStatic
+{
+    public class User
+    {
+        private static int _id;
+        private string _pass;
+        public int Id { get; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password
+        {
+            get => _pass;
+            set
+            {
+                if (PasswordChecker(value))
+                {
+                    _pass = value;
+                }
+            }
+        }
+        public User(string fullName, string email, string pass)
+        {
+            Id = ++_id;
+
+            FullName = fullName;
+            Email = email;
+            Password = pass;
+        }
+        public static bool PasswordChecker(string pass)
+        {
+            if (pass.Length < 8)
+            {
+                return false;
+            }
+
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasDigit = false;
+
+            foreach (char c in pass)
+            {
+                if (char.IsUpper(c))
+                    hasUpper = true;
+                if (char.IsLower(c))
+                    hasLower = true;
+                if (char.IsDigit(c))
+                    hasDigit = true;
+
+                if (hasUpper && hasLower && hasDigit)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Id} {FullName} {Email}");
+        }
+        public static User FindUserById(User[] users, int id)
+        {
+            foreach (var user in users)
+            {
+                if (user.Id == id)
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
+    }
+}
